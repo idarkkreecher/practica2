cdc21a9c367f8e0c8e934e2ec9907744de0672c5
Bryan Garcia <kreecher170797@outlook.com>
2017-10-23 11:17:46 -0500

    v0.2 Agregado funcion factorial

--- practica2.cpp
+++ practica2.cpp
@@ -2,6 +2,20 @@
 
 using namespace std;
 
+int factorial(int num1);
+
 int main(){
-	cout<<"Hola Mundo!";
+	int num1;
+	cout<<"Inserta un numero para sacar su factorial: ";
+	cin>>num1;
+	cout<<"El factorial de "<<num1<<" es "<<factorial(num1)<<endl;
+	system("PAUSE>nul");
+}
+
+int factorial(int num1){
+	int acum=1;
+	for (int i=1; i<=num1; i++){
+		acum=acum*i;
+	}
+	return acum;
 }
